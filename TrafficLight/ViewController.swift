901b352e4a13dcde83826f6b7db6290188883d7d
Zininas <zininas@mail.ru>
2023-11-12 17:19:28 +0300

    Bug fixes

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -14,31 +14,45 @@ final class ViewController: UIViewController {
     @IBOutlet weak var greenTrafficLight: UIView!
     @IBOutlet weak var trafficLightButton: UIButton!
     
+    private let signalIsOn = 1.0
+    private let signalIsOff = 0.3
+    
+    private var tapCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        redTrafficLight.layer.cornerRadius = redTrafficLight.frame.size.width / 2
-        redTrafficLight.alpha = 0.3
-        yellowTrafficLight.layer.cornerRadius = yellowTrafficLight.frame.size.width / 2
-        yellowTrafficLight.alpha = 0.3
-        greenTrafficLight.layer.cornerRadius = greenTrafficLight.frame.size.width / 2
-        greenTrafficLight.alpha = 0.3
+        redTrafficLight.layer.cornerRadius = redTrafficLight.frame.height / 2
+        yellowTrafficLight.layer.cornerRadius = yellowTrafficLight.frame.height / 2
+        greenTrafficLight.layer.cornerRadius = greenTrafficLight.frame.height / 2
+        
+        redTrafficLight.alpha = signalIsOff
+        yellowTrafficLight.alpha = signalIsOff
+        greenTrafficLight.alpha = signalIsOff
+        
         trafficLightButton.layer.cornerRadius = 15
     }
     @IBAction func trafficLightButtonDidTapped() {
-
         if trafficLightButton.title(for: .normal) == "START" {
-            redTrafficLight.alpha = 1
+
             trafficLightButton.setTitle("NEXT", for: .normal)
-        } else if redTrafficLight.alpha == 1 {
-            redTrafficLight.alpha = 0.3
-            yellowTrafficLight.alpha = 1
-        } else if yellowTrafficLight.alpha == 1 {
-            yellowTrafficLight.alpha = 0.3
-            greenTrafficLight.alpha = 1
-        } else {
-            greenTrafficLight.alpha = 0.3
-            redTrafficLight.alpha = 1
+            }
+        
+        tapCount += 1
+
+        switch tapCount {
+        case 1,4,7,10:
+            redTrafficLight.alpha = signalIsOn
+            greenTrafficLight.alpha = signalIsOff
+        case 2,5,8,11:
+            redTrafficLight.alpha = signalIsOff
+            yellowTrafficLight.alpha = signalIsOn
+        case 3,6,9,12:
+            yellowTrafficLight.alpha = signalIsOff
+            greenTrafficLight.alpha = signalIsOn
+        default:
+            break
         }
+
     }
 }
 
